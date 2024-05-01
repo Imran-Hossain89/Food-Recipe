082ac020459e981731d38329501e498b3009090d
Imran Hossain <imran.kst1989@gmail.com>
2024-05-02 00:32:09 +0600

    initialized

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import Popular from "../Components/Popular";
+import Veggie from "../Components/Veggie";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+            <Popular/>
+            <Veggie/>
+    </div>
+  )
+}
+
+export default Home
